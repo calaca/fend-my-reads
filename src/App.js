4d954539ffb58839c9a1e943a3bb1c43f8ff14dd
calaca <lorena.calaca.od@gmail.com>
2018-02-09 11:27:28 -0200

    creat react app + first component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Top from './Top';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Top />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Top.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Top extends Component {
+  render() {
+    return (
+      <div className="top">
+        <h1>My Reads</h1>
+      </div>
+    )
+  }
+}
+
+export default Top;
