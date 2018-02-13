b7d63b749ac43977c4ddcbe749233e2ef9ccbf19
calaca <lorena.calaca.od@gmail.com>
2018-02-13 14:44:03 -0200

    tests: add enzyme

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
+import { shallow, mount } from 'enzyme';
 import App from '../components/App/App';
 import { body, init } from '../__mocks__/MockData';
 
-it('renders without crashing', () => {
-  /**
-   * fetch.mockResponse(body, init) - Mock all fetch calls
-   * Source: https://github.com/jefflau/jest-fetch-mock#using-with-create-react-app
-   */
-  fetch.mockResponse(JSON.stringify(body), { init });
-  const div = document.createElement('div');
-  ReactDOM.render(
-    /**
-     * A <Router> that keeps the history of your "URL" in memory (does not read or write to the address bar). Useful in tests and non-browser environments like React Native.
-     * This was necessary because App's Routes needed a wrapping Router.
-     * Source: https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/api/MemoryRouter.md#memoryrouter
-     */
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-    div
-  );
+// TODO: add full rendering tests to check ensure components integrate correctly
+describe('App', () => {
+  it('renders without crashing', () => {
+    fetch.mockResponse(JSON.stringify(body), { init });
+    shallow(<MemoryRouter><App /></MemoryRouter>);
+  });
+  
+  it('calls componentDidMount', () => {
+    const spy = jest.spyOn(App.prototype, 'componentDidMount');
+    const wrapper = mount(<MemoryRouter><App /></MemoryRouter>);
+    expect(spy).toHaveBeenCalledTimes(1);
+    spy.mockClear();
+  });
 });
+
+/**
+ * NOTES:
+ * 
+ * fetch.mockResponse(body, init) - Mock all fetch calls
+ * Source: https://github.com/jefflau/jest-fetch-mock#using-with-create-react-app
+ * ===
+ * A <Router> that keeps the history of your "URL" in memory (does not read or write to the address bar). Useful in tests and non-browser environments like React Native.
+ * This was necessary because App's Routes needed a wrapping Router.
+ * Source: https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/api/MemoryRouter.md#memoryrouter
+ * ===
+ * Enzyme was used here to test components in isolation from the child components they render.
+ * Source: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#testing-components
+ */
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,11 @@
  * Both only available in the browser but not in Node environments
  */
 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 const localStorageMock = {
   getItem: jest.fn(),
   setItem: jest.fn(),
